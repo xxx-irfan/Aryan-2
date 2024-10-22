354080417dec1153a76ac1b9fe0ddf32b9b26b35
YOUTUBE-TACH <newgithub@yandex.com>
2024-10-22 08:04:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ logger("Opened server site...", "[ Starting ]");
 function startBot(message) {
     (message) ? logger(message, "[ Starting ]") : "";
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Priyansh.js"], {
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "ARIF-BABU.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
